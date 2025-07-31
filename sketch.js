c88ae0939cc31718df986b427f8b2d61ce23c6d1
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-07-30 21:11:07 -0500

    Add createGrid()

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// grid.replaceChildren();
+
+const MAX_GRID_WIDTH = 960;
+const grid = document.querySelector("#grid");
+grid.style.maxWidth = MAX_GRID_WIDTH + "px";
+
+function createGrid (grid, cols) {      
+    let divSize = Math.floor(MAX_GRID_WIDTH / cols);    
+    
+    // Create the divs
+    for(let i = 0; i < cols * cols; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("cell");
+        newDiv.style.height = divSize + "px";
+        newDiv.style.width = divSize + "px";
+        grid.appendChild(newDiv);
+    }
+}
+
+createGrid(grid, 16);
